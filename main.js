4f5915098e8b5b190257485f91aff7f1cff95017
CloverJ644 <lilly.johnson@stu.ocsb.ca>
2024-10-15 15:14:55 +0000

    1114/10/15

--- main.js
+++ main.js
@@ -11,3 +11,44 @@
 
 'use strict';
 
+function square(value) {
+    let answer = value**2;
+}
+
+let x = square(5);   // 5 squared is 25
+console.log(x);
+
+//Converts a value to Fahrenheit
+function to_fahrenheit(celsius) {
+    let f = (Math.round((celsius * 9/5) + 32));
+    return f;
+}
+
+//Aaand vice-versa for Celsius!
+function to_celsius(fahrenheit) {
+    let c = (Math.round((fahrenheit - 32) * 5/9));
+    return c;
+}
+
+//DICE TIMEEEE!!! :D
+function roll_dice(qty, sides) {
+    let result = qty*(Math.ceil(Math.random() * (sides)));
+    return result;
+}
+//Wait what.
+//Yeah I don't know enough to code this :(
+
+//Dice time is over :(((
+//Anyways cricle time???
+function area_circle(radius) {
+    let area = Math.PI*(radius**2);
+    return area;
+}
+
+//And cylinder ig
+function volume_cylinder(radius, height) {
+    area_circle(radius);
+    let area = Math.PI*(radius**2);
+    let volume = area*height;
+    return volume;
+}
